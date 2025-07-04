e468689cc5674c4f0dcb5160a470e3d4577eaa40
HugoCLSC <hugo.santos.costa@gmail.com>
2025-07-04 15:40:02 +0100

    Refactor: various fixes

--- extras/bucket.py
+++ extras/bucket.py
@@ -10,10 +10,16 @@ class Bucket:
         self.toolhead = None
 
         self.printer.register_event_handler("klippy:ready", self.handle_ready)
-        self.printer.register_event_handler("klippy:connect", self.handle_connect)
+        self.printer.register_event_handler(
+            "klippy:connect", self.handle_connect
+        )
 
-        self.has_custom_bed_bound = config.getboolean("has_custom_boundary", False)
-        self.bucket_position = config.getfloatlist("bucket_position", None, count=2)
+        self.has_custom_bed_bound = config.getboolean(
+            "has_custom_boundary", False
+        )
+        self.bucket_position = config.getfloatlist(
+            "bucket_position", None, count=2
+        )
         self.travel_speed = config.getfloat(
             "travel_speed", default=50.0, minval=20.0, maxval=10000.0
         )
@@ -29,26 +35,33 @@ class Bucket:
     def handle_ready(self):
         if self.has_custom_bed_bound:
             self.custom_bed_bound_object = self.printer.lookup_object(
-                "bed_custom_bound"
+                "bed_custom_bound", default=None
             )
 
     def handle_connect(self):
-        self.toolhead = self.printer.lookup_object("toolhead")
+        self.toolhead = self.printer.lookup_object("toolhead", default=None)
 
     def move_to_bucket(self, split: typing.Optional["bool"] = False):
         """Moves to bucket position"""
-        if self.toolhead is None:
+        if not self.toolhead:
             return
         try:
-            # * Check if the bucket is outside the printer boundary
-            if self.custom_bed_bound_object is not None:
-                _conf_bound = self.custom_bed_bound_object.check_boundary_limits(
-                    position=(self.bucket_position[0], self.bucket_position[1])
+            if self.custom_bed_bound_object:
+                _conf_bound = (
+                    self.custom_bed_bound_object.check_boundary_limits(
+                        position=(
+                            self.bucket_position[0],
+                            self.bucket_position[1],
+                        )
+                    )
                 )
                 if (
                     not _conf_bound["x"] or not _conf_bound["y"]
-                ) and self.custom_bed_bound_object.get_status()["status"] == "custom":
+                ) and self.custom_bed_bound_object.get_status().get(
+                    "status", ""
+                ) == "custom":
                     self.custom_bed_bound_object.restore_default_boundary()
+                    
 
             if not split:
                 self.toolhead.manual_move(
@@ -56,7 +69,9 @@ class Bucket:
                     self.travel_speed,
                 )
             else:
-                self.toolhead.manual_move([self.bucket_position[0]], self.travel_speed)
+                self.toolhead.manual_move(
+                    [self.bucket_position[0]], self.travel_speed
+                )
                 self.toolhead.wait_moves()
                 self.toolhead.manual_move(
                     [self.bucket_position[0], self.bucket_position[1]],
@@ -66,8 +81,9 @@ class Bucket:
             self.toolhead.wait_moves()
 
             if (
-                self.custom_bed_bound_object is not None
-                and self.custom_bed_bound_object.get_status()["status"] == "default"
+                self.custom_bed_bound_object
+                and self.custom_bed_bound_object.get_status().get("status", "")
+                == "default"
             ):
                 self.custom_bed_bound_object.set_custom_boundary()
         except Exception as e:
